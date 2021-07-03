9645dda022bb8daf2a30043415dde4590aad7010
Juliano Heqimaj <julianoheqimaj@Julianos-MBP.lan>
2021-07-03 12:09:26 -0400

    hw#1

---
+++ src/HomeWorkPackage/HW1.java
@@ -0,0 +1,37 @@
+package HomeWorkPackage;
+
+public class HW1 {
+    public static void main(String[] args) {
+        System.out.println("hw1");
+
+// 1. number of hours in a day (byte)
+        byte hoursPerDay = 24;
+
+
+// 2. max no. of days in an year
+        short daysPerYear = 365;
+
+// 3. total number of employees in an organization (IBM)
+        int numberOfEmployees = 345900;
+
+// 4. population in a country (USA)
+        long usaPopulation = 331002651L;
+
+
+// 5. mortgage rate
+        float mortgageRate = 2.8f;
+
+
+// 6. balance in a bank account
+        short accountBalance = 30000;
+
+
+// 7. does the sun rise from the west? Store answer
+        boolean sunRiseFromTheWest = false;
+
+// 8. initials of your name (^Use: char)
+// My initials : J.H
+
+
+
+}
