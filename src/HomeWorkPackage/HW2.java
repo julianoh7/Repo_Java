55ca1b572f27403293f0d603c6a6785026ca29ac
Juliano Heqimaj <julianoheqimaj@Julianos-MBP.lan>
2021-07-06 20:46:41 -0400

    HW2

---
+++ src/HomeWorkPackage/HW2.java
@@ -0,0 +1,72 @@
+package HomeWorkPackage;
+
+public class HW2 {
+    public static void main(String[] args) {
+        System.out.println("HW2");
+
+    //* Convert deg-Cel into Fahrenheit
+    //* fT = cT * 9/5 +32
+
+    double cTemp = 21.11;
+    double fTemp = cTemp * 9/5 + 32;
+    double kTemp = cTemp + 273.15;
+
+
+    //* C->F
+        System.out.println(cTemp + "°C = " + fTemp + "°F");
+    //* C->K
+        System.out.println(cTemp +"°C = " + kTemp + "K");
+    //* F->C
+        System.out.println(fTemp + "°F = " + cTemp);
+    //* F->K
+        System.out.println(fTemp + "°F = " + kTemp);
+    //* K->C
+        System.out.println(kTemp + "K = " + cTemp);
+    //* K ->F
+        System.out.println(kTemp + "K = " + fTemp);
+
+    //*myInitial ="J.H";
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
