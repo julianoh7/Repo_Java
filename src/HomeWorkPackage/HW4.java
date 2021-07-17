3d9e5152face101906a3a39e7841da930a2d718d
Juliano Heqimaj <julianoheqimaj@Julianos-MBP.lan>
2021-07-17 01:57:44 -0400

    HW4

---
+++ src/HomeWorkPackage/HW4.java
@@ -0,0 +1,80 @@
+package HomeWorkPackage;
+
+import java.util.Arrays;
+import java.util.Locale;
+
+public class HW4 {
+    public static void main(String[] args) {
+        System.out.println("HW#4");
+
+
+        // Question - 1:
+        /**
+         * String threeWordSentence = "hApPy nEW yeAr"
+         * // code
+         * String threeWordSentence = "Happy New Year"
+         */
+        String threeWordSentence_1 = "hApPy nEW yeAr";
+        String[] threeWordSentence_1Split = threeWordSentence_1.split(" ");
+        System.out.println(Arrays.toString(threeWordSentence_1Split));
+        String t = threeWordSentence_1Split[0].substring(0,1).toUpperCase() + threeWordSentence_1Split[0].substring(1).toLowerCase();
+        String t1 = threeWordSentence_1Split[1].substring(0,1).toUpperCase() + threeWordSentence_1Split[1].substring(1).toLowerCase();
+        String t2 = threeWordSentence_1Split[2].substring(0,1).toUpperCase() + threeWordSentence_1Split[2].substring(1).toLowerCase();
+        System.out.println("Before Code : " + threeWordSentence_1);
+        System.out.println("After Code : " + t + " " + t1 + " " + t2 + "\n");
+
+
+
+
+
+        // Question - 2:
+
+        String sentence2 = "Very Good morning";
+        String [] sentence2Split = sentence2.split(" ");
+        String a = sentence2Split[0].substring(0,1).toUpperCase() + sentence2Split[1].substring(0,1).toUpperCase() + sentence2Split[2].substring(0,1).toUpperCase();
+        System.out.println("Before abbreviation : " + sentence2);
+        System.out.println("After abbreviation : " + a + "\n");
+
+
+
+
+
+        // Question - 3:
+        /**
+         * print the number of words in the sentence.
+         */
+
+        String sentence_3 = "We all are here to learn programming and testing";
+        String[] sentence_3Split = sentence_3.split(" ");
+        System.out.println(Arrays.toString(sentence_3Split));
+        int sentence_3Length = sentence_3Split.length;
+        System.out.println("The number of words in the sentence is : " + sentence_3Length);
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
